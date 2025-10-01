09f28dcf2e844a04a9a4cc28aecd98490ac13b88
chris-laur <christielaur@gmail.com>
2025-10-01 08:36:23 -0400

    Update data.js
    Comment out Axios code

--- src/services/data.js
+++ src/services/data.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
 
+// TODO: Update code to address Axios vulnerabilities
+
 const API_URL = import.meta.env.DEV ? '/api' : 'https://petdeskapi2.azurewebsites.net/api';
 
 export function getAppointmentChangeRequestData() {
-    return axios.get(API_URL + '/appointmentchangerequests');
+    return null;
+    //return axios.get(API_URL + '/appointmentchangerequests');
 }
 
 export function saveAppointmentChangeRequestData(request) {
-    return axios.put(API_URL + '/updateappointmentchangerequest/' + request.appointmentId, {
-        status: request.status,
-        requestedDateTimeOffset: request.requestedDateTimeOffset,
-    });
+    return null;
+    //return axios.put(API_URL + '/updateappointmentchangerequest/' + request.appointmentId, {
+        //status: request.status,
+        //requestedDateTimeOffset: request.requestedDateTimeOffset,
+    //});
 }
